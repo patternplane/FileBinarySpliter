36b7c09c4c3947187f062361c35acbcdc0ee6245
patternplane <bss011202@naver.com>
2022-05-06 04:28:02 +0900

    [FEAT] 기초 입력받기 구현

--- BinarySpliter/main.cpp
+++ BinarySpliter/main.cpp
@@ -8,6 +8,16 @@ int main() {
 	// text.txt 파일을 out.txt 파일로 암호화합니다.
 	// encryptor::Lock("text.txt", "out.txt");
 
+	std::string inputPath;
+	std::string outputPath;
+	
+	std::cout << "입력파일 이름을 입력하세요 : \r\n";
+	std::cin >> inputPath;
+	std::cout << "출력파일 이름을 입력하세요 : \r\n";
+	std::cin >> outputPath;
+
+	encryptor::Lock(inputPath,outputPath);
+
 	return 0;
 }
 
